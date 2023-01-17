f59f4c8e4269dfea31dee2b815b800f77ebc1acd
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-01-17 02:25:46 +0100

    Export to excel
    Using openpyxl library to export the data to an excel file

--- CMCToExcel.py
+++ CMCToExcel.py
@@ -1,6 +1,12 @@
 from requests import Request, Session
+from openpyxl import Workbook
 import pprint
 import json
+
+#creation du fichier excel
+wb = Workbook()
+#feuille 1
+ws = wb.active
 
 key = input("Saisissez votre clé d'API (https://pro.coinmarketcap.com/account) : ")
 quotesLatestUrl = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
@@ -20,6 +26,10 @@ headers = {
 session = Session()
 session.headers.update(headers)
 response = session.get(quotesLatestUrl, params = data)
-
 price = json.loads(response.text)['data'][crypto][0]['quote']['USD']['price']
 pprint.pprint("Prix actuel de "+crypto+" : " + str(round(price,2)) + " $.")
+
+# ajout d'une ligne contenant la devise et le prix correspondant
+ws.append(["DEVISE","PRIX"])
+ws.append(["$"+crypto, price])
+wb.save('prices.xlsx')
