003b073825ea79c8a1df0d4198cc3314d54d3f97
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-01-17 03:19:48 +0100

    Excel sheet customization
    font, cell size, colors...

--- CMCToExcel.py
+++ CMCToExcel.py
@@ -1,5 +1,8 @@
 from requests import Request, Session
 from openpyxl import Workbook
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import PatternFill
 import pprint
 import json
 
@@ -8,6 +11,8 @@ wb = Workbook()
 #feuille 1
 ws = wb.active
 ws.append(["DEVISE","PRIX","MARKETCAP","VOLUME SOUS 24H"])
+for cell in ws[1]:
+    cell.font = Font(name='Arial', bold=True)
 
 key = input("Saisissez votre clé d'API (https://pro.coinmarketcap.com/account) : ")
 quotesLatestUrl = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
@@ -15,6 +20,7 @@ quotesLatestUrl = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/la
 #tableau des cryptomonnaies ciblées, à modifier en dur dans le script
 devises = ("BTC","ETH","BNB","USDT","XRP","ADA","ORAI","MATIC","DOT","AVAX","TRX","UNI","ATOM","LINK")
 
+#récupération des données et enregistrement dans la feuille excel
 for crypto in devises:
 	data = {
 		'symbol':crypto,
@@ -32,4 +38,37 @@ for crypto in devises:
 	volume24h = json.loads(response.text)['data'][crypto][0]['quote']['USD']['volume_24h']
 	# ajout d'une ligne contenant la devise et le prix correspondant
 	ws.append(["$"+ crypto, price, marketcap,volume24h])
-	wb.save('data.xlsx')
+
+#personnalisation de la feuille excel
+#taille automatique des colonnes
+for col in ws.columns:
+    max_length = 0
+    column = col[0].column
+    for cell in col:
+        try:
+            if len(str(cell.value)) > max_length:
+                max_length = len(cell.value)
+        except:
+            pass
+    adjusted_width = (max_length + 2) * 1.2
+    ws.column_dimensions[get_column_letter(column)].width = adjusted_width
+
+#on colore la colonne contenant les devises
+for row in ws.iter_rows(min_row=2, min_col=1, max_col=1, max_row=ws.max_row):
+    for cell in row:
+        cell.fill = PatternFill(start_color="B0F2B6", end_color="B0F2B6", fill_type = "solid")
+
+#entêtes
+for cell in ws[1]:
+    cell.fill = PatternFill(start_color="096A09", end_color="096A09", fill_type = "solid")
+for cell in ws[1]:
+    cell.font = Font(color="FFFFFF")
+for row in ws.iter_rows():
+    for cell in row:
+        cell.font = Font(size=14)
+
+
+
+wb.save('data.xlsx')
+    
+    
