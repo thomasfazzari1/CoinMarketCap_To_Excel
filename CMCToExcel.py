3ea59108e498b929b182c76c2eeb5ef80c599f58
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-01-17 02:37:13 +0100

    Various cryptocurrencies

--- CMCToExcel.py
+++ CMCToExcel.py
@@ -7,29 +7,27 @@ import json
 wb = Workbook()
 #feuille 1
 ws = wb.active
+ws.append(["DEVISE","PRIX"])
 
 key = input("Saisissez votre clé d'API (https://pro.coinmarketcap.com/account) : ")
 quotesLatestUrl = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
 
-crypto = input("Saisissez la devise (ex: BTC): ")
+#tableau des cryptomonnaies ciblées, à modifier en dur dans le script
+devises = ("BTC","ETH","BNB","USDT","XRP","ADA","ORAI","MATIC","DOT","AVAX","TRX","UNI","ATOM","LINK")
 
-data = {
-	'symbol':crypto,
-	'convert':'USD'
-}
-
-headers = {
-	'Accepts':'application/json',
-	'X-CMC_PRO_API_KEY':key
-}
-
-session = Session()
-session.headers.update(headers)
-response = session.get(quotesLatestUrl, params = data)
-price = json.loads(response.text)['data'][crypto][0]['quote']['USD']['price']
-pprint.pprint("Prix actuel de "+crypto+" : " + str(round(price,2)) + " $.")
-
-# ajout d'une ligne contenant la devise et le prix correspondant
-ws.append(["DEVISE","PRIX"])
-ws.append(["$"+crypto, price])
-wb.save('prices.xlsx')
+for crypto in devises:
+	data = {
+		'symbol':crypto,
+		'convert':'USD'
+	}
+	headers = {
+		'Accepts':'application/json',
+		'X-CMC_PRO_API_KEY':key
+	}
+	session = Session()
+	session.headers.update(headers)
+	response = session.get(quotesLatestUrl, params = data)
+	price = json.loads(response.text)['data'][crypto][0]['quote']['USD']['price']
+	# ajout d'une ligne contenant la devise et le prix correspondant
+	ws.append(["$"+crypto, price])
+	wb.save('prices.xlsx')
